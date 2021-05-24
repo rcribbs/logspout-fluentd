70c263d65a8ec9673e99a9516b33410dee1d6d24
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2021-05-24 14:31:05 -0700

    Remove build.sh.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-mkdir -p /go/src/github.com/gliderlabs
-cp -r /src /go/src/github.com/gliderlabs/logspout
-
-# backwards compatibility
-ln -fs /tmp/docker.sock /var/run/docker.sock
